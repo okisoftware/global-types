c053000f425e4381146fb13396957267da1fa13a
Goekay Pamuk <pag@informatec.com>
2025-03-28 20:57:32 +0100

    update

--- dist/types/message.d.ts
+++ dist/types/message.d.ts
@@ -1,9 +1,11 @@
 import { MessageType } from "../enums/message-type";
+import { User } from "./user";
 export type Message = {
     _id: string;
     chatId: string;
     userID: string;
     payload: string;
+    participants?: Array<User>;
     type: MessageType;
     ts: number;
     feAttr?: {
